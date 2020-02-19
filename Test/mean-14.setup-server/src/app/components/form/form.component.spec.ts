bc4fb193a2beba9802b027dc4028b9346872e8f6
daacarva <daacarva@bancolombia.com.co>
2020-02-19 16:20:24 -0500

    probando front end

--- Test/mean-14.setup-server/src/app/components/form/form.component.spec.ts
+++ Test/mean-14.setup-server/src/app/components/form/form.component.spec.ts
@@ -1,16 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { of } from 'rxjs';
+import { NavigationService } from 'src/app/services/navigation.service';
+import { MatSnackBar } from '@angular/material';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormArray } from '@angular/forms';
 
-describe('FormComponent', () => {
+
+class RepositoryServiceStub {
+  savePins() {
+    return of(true);
+  }
+}
+
+class NavigationServiceStub {
+  goToPins() {
+
+  }
+}
+
+class MatSnackBarStub {
+  open() {
+    return {
+      afterDismissed: () => {
+        return of(true);
+      }
+    }
+  }
+}
+
+
+fdescribe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [FormComponent],
+      providers: [
+        { provide: RepositoryService, useClass: RepositoryServiceStub },
+        { provide: NavigationService, useClass: NavigationServiceStub },
+        { provide: MatSnackBar, useClass: MatSnackBarStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +59,43 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("when component is initialized", () => {
+    it('Should create the forms', () => {
+      expect(Object.keys(component.firstFormGroup.controls)).toEqual(['title', 'author', 'description']);
+      expect(Object.keys(component.secondFormGroup.controls)).toEqual(['firstAsset', 'assets']);
+    });
+  })
+
+  describe('when addAsset is executed', () => {
+    it('Should add new group', () => {
+      const asset = <FormArray>component.secondFormGroup.get('assets');
+      component.addAsset();
+      component.addAsset();
+      expect(Object.keys(asset.controls)).toEqual(['0', '1']);
+    })
+  })
+
+  describe('When deleteAsset', () => {
+    it('should remove the form control', () => {
+      const asset = <FormArray>component.secondFormGroup.get('assets');
+      component.addAsset();
+      component.deleteAsset(0);
+      expect(Object.keys(asset.controls)).toEqual([]);
+    });
+
+    describe('when savePins is executed', () => {
+      it('shoul nabigate to pins view', () => {
+        const navigate = spyOn((<any>component).navigate, 'goToPins');
+        const open = spyOn((<any>component).snackBar,'open').and.callThrough();
+
+        component.savePin();
+        expect(navigate).toHaveBeenCalled();
+        expect(open).toHaveBeenCalledWith('Your pin is saved, Redirecting ...', 'Cool!', {
+          duration: 2000
+        });
+      })
+    })
+
+  })
 });
--- Test/mean-14.setup-server/src/app/components/menu/menu.component.spec.ts
+++ Test/mean-14.setup-server/src/app/components/menu/menu.component.spec.ts
@@ -1,16 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { By } from '@angular/platform-browser';
 
-describe('MenuComponent', () => {
+fdescribe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [MenuComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -20,6 +21,16 @@ describe('MenuComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title.nativeElement.innerHTML).toBe('eLearning Management System');
   });
+
+  it('Testing output', () => {
+    const val = true;
+    component.clicked.subscribe((result) => {
+      expect(result).toBe(val);
+    })
+    component.clicked.next(val);
+  });
+
 });
