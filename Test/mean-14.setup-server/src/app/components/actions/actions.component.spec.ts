3c06b7576232a8d3de05cbe8332ba8c68d6c57c9
daacarva <daacarva@bancolombia.com.co>
2020-02-21 17:07:16 -0500

    probando nuevas clases

--- Test/mean-14.setup-server/src/app/components/actions/actions.component.spec.ts
+++ Test/mean-14.setup-server/src/app/components/actions/actions.component.spec.ts
@@ -1,16 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionsComponent } from './actions.component';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material';
+import { PinsService } from '../pins/pins.service';
 
-describe('ActionsComponent', () => {
+class MatBottomSheetRefStub {
+  dismiss() { };
+}
+
+class PinsServiceStub {
+  resolveActionObserver() { }
+}
+
+fdescribe('ActionsComponent', () => {
   let component: ActionsComponent;
   let fixture: ComponentFixture<ActionsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActionsComponent ]
+      declarations: [ActionsComponent],
+      providers: [
+        { provide: MatBottomSheetRef, useClass: MatBottomSheetRefStub },
+        { provide: PinsService, useClass: PinsServiceStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +38,18 @@ describe('ActionsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call openLink', () => {
+    const mouseEvent = new MouseEvent('click');
+    // const eventCall = spyOn(event, 'preventDefault').and.callFake(() => { });
+    const dismissCall = spyOn((<any>component).bottomSheetRef, 'dismiss');
+    const resolveActionObserverCall = spyOn((<any>component).pinsService, 'resolveActionObserver');
+
+    component.openLink(mouseEvent, '');
+    // expect(eventCall).toHaveBeenCalled();
+    expect(dismissCall).toHaveBeenCalled();
+    expect(resolveActionObserverCall).toHaveBeenCalled();
+
+
+  })
 });
--- Test/mean-14.setup-server/src/app/components/layout/layout.component.spec.ts
+++ Test/mean-14.setup-server/src/app/components/layout/layout.component.spec.ts
@@ -1,16 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { MatBottomSheet } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ActionsComponent } from '../actions/actions.component';
 
-describe('LayoutComponent', () => {
+class MatBottomSheetStub {
+  open() { };
+}
+
+fdescribe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      declarations: [LayoutComponent],
+      providers: [{ provide: MatBottomSheet, useClass: MatBottomSheetStub }],
+      imports: [RouterTestingModule.withRoutes([{
+        path: '',
+        component: LayoutComponent
+      },
+      {
+        path: 'app/add',
+        component: LayoutComponent
+      }
+      ])],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +41,35 @@ describe('LayoutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the editMode to false', () => {
+    const veryfyEditMode = spyOn(component, 'verifyEditMode').and.callThrough();
+    fixture.ngZone.run(() => {
+      (<any>component).router.navigate(['/']);
+
+      fixture.whenStable().then(() => {
+        expect(component.editMode).toBeFalsy();
+        expect(veryfyEditMode).toHaveBeenCalled();
+      })
+    })
+  })
+
+  it('should set the editMode to true', () => {
+    const veryfyEditMode = spyOn(component, 'verifyEditMode').and.callThrough();
+    fixture.ngZone.run(() => {
+      (<any>component).router.navigate(['app/add']);
+
+      fixture.whenStable().then(() => {
+        expect(component.editMode).toBeTruthy();
+        expect(veryfyEditMode).toHaveBeenCalled();
+      })
+    })
+  })
+
+  it('should call the open method', () => {
+    const open = spyOn((<any>component).bottomSheet, 'open');
+    component.openBottomSheet();
+    expect(open).toHaveBeenCalledWith(ActionsComponent);
+  })
+
 });
